ae963426a266417ddc196545b31daae3c3bc29e3
Marvel Dev <marvellousalika48@gmail.com>
2020-09-30 15:43:33 +0100

    - add class constructors.

---
+++ class.js
@@ -0,0 +1,49 @@
+class FactBook {
+  numberFact() {
+    const input = document.querySelector('.number-input');
+    input.addEventListener('input', () => {
+      const inputValue = input.value.trim();
+      if (inputValue.length >= 1 ) {
+        fetch('http://numbersapi.com/'+inputValue)
+        .then((response) => {
+          return response.text();
+        })
+        .then ((data) => {
+          document.getElementById('number-fact').innerHTML = data;
+        }) 
+        .catch(() => {
+          document.getElementById('number-fact').innerHTML = 'failed to fetch data';
+        });
+      } else {
+        document.getElementById('number-fact').innerHTML = '';
+      }
+    })
+  }
+
+  dateFact() {
+    const dateInput = document.querySelector('#date-input');
+    dateInput.addEventListener('input', () => {
+      const dateInputValue = dateInput.value.trim();
+      const dateValue = dateInputValue.split('-').join('/');
+      const newDateValue = dateValue.slice(5);
+      if (dateInputValue.length >= 1 ) {
+        fetch('http://numbersapi.com/'+newDateValue)
+        .then((response) => {
+            return response.text();
+        })
+        .then ((data) => {
+          document.getElementById('date-fact').innerHTML = data;
+        })
+        .catch(() => {
+          document.getElementById('date-fact').innerHTML = 'failed to fetch data';
+        })
+      } else {
+        document.getElementById('date-fact').innerHTML ='';
+      }
+    })
+  }
+}
+
+const fact = new FactBook();
+fact.numberFact();
+fact.dateFact();
--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const input = document.querySelector('.number-input');
+const dateInput = document.querySelector('#date-input');
 
 input.addEventListener('input', () => {
   const inputValue = input.value.trim();
@@ -8,9 +9,32 @@ input.addEventListener('input', () => {
         return response.text();
     })
     .then ((data) => {
-      document.querySelector('.fact-output').innerHTML = data;
+      document.getElementById('number-fact').innerHTML = data;
+    }) 
+    .catch(() => {
+      document.getElementById('number-fact').innerHTML = 'failed to fetch data';
+    });
+  } else {
+    document.getElementById('number-fact').innerHTML = '';
+  }
+})
+
+dateInput.addEventListener('input', () => {
+  const dateInputValue = dateInput.value.trim();
+  const dateValue = dateInputValue.split('-').join('/');
+  const newDateValue = dateValue.slice(5);
+  if (dateInputValue.length >= 1 ) {
+    fetch('http://numbersapi.com/'+newDateValue)
+    .then((response) => {
+        return response.text();
+    })
+    .then ((data) => {
+      document.getElementById('date-fact').innerHTML = data;
+    })
+    .catch(() => {
+      document.getElementById('date-fact').innerHTML = 'failed to fetch data';
     })
   } else {
-    document.querySelector('.fact-output').innerHTML ='';
+    document.getElementById('date-fact').innerHTML ='';
   }
 })
